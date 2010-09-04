1c217827d09dcb4196c807d796b0565de3ab2736
Noah Lavine <nlavine@haverford.edu>
2010-09-03 22:04:59 -0400

    New JIT Approach
    I'm starting a new approach where instead of trying to make Guile boot with a
    test JIT function operating on every Scheme call, I'm going to have the JIT
    just fail on all functions, which will lead to Guile booting fine. The next
    step will be to start JITing functions one VM instruction at a time, which
    will hopefully provide a better development path than the last approach.

--- libguile/vm-jit.c
+++ libguile/vm-jit.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <sys/mman.h> /* for mprotect */
+#include <stdlib.h> /* for posix_memalign */
+#include <unistd.h> /* for getpagesize */
 
 #include "libguile/instructions.h"
 #include "libguile/snarf.h"
@@ -11,9 +14,14 @@
 #include "libguile/numbers.h"
 
 #include "libguile/vm-jit.h" /* defines struct scm_jit_code */
-#include "lightning.h"
+#include "libguile/lightning/lightning.h"
 
 /* SCM jitcode_vtable = NULL; */
+
+void my_print(char *msg)
+{
+  printf(msg);
+}
 
 /* jit_objcode: this procedure either JITs the given object code, and returns a
  * pointer to an scm_jit_code object with the results, or SCM_BOOL_F if it
@@ -22,34 +30,55 @@
 jitf *jit_objcode(struct scm_objcode *objcode)
 {
   /* SCM code; */
-  jitf *code;
+  jitf code, *block_pointers;
   jit_insn *buffer, *end;
+  int memalign_res, mprotect_res;
 
-  buffer = scm_gc_calloc(32 * sizeof (jit_insn), "JIT instruction buffer");
-  if (buffer == NULL)
+  if (objcode->len > 0) {
+    /* this is too complicated for us to JIT! :-) */
+    return SCM_BOOL_F;
+  }
+  /* else, the real content of this function ... */
+
+  /* this is an absurd allocation strategy. It can be fixed once the JIT is working. */
+  memalign_res = posix_memalign(&buffer, getpagesize(), getpagesize());
+  if (memalign_res != 0) {
+    perror("posix_memalign");
     return (jitf *)SCM_BOOL_F;
+  }
 
   /* Make a test function */
   jit_set_ip(buffer);
   jit_prolog(3);
+  /*
   jit_movi_p(JIT_R0, "JIT code called!\n");
   jit_prepare(1);
   jit_pusharg_p(JIT_R0);
-  jit_finish(printf);
+  jit_finish(my_print);
+  */
   jit_ret();
 
   end = (jit_insn *)jit_get_ip().ptr;
   jit_flush_code(buffer, end);
 
-  /* Make the struct */
-  /* code = scm_c_make_structv(jitcode_vtable, 1, 1, (scm_t_bits *)buffer); */
-  code = scm_gc_malloc (sizeof (jitf *), "a table of pointers to machine code");
-  if (code == NULL)
+  /* although it is technically the identity, this cast should make the JIT code
+   * play as nicely with the C type system as we can expect. */
+  code = (jitf)buffer;
+  block_pointers = scm_gc_malloc (sizeof (jitf *),
+                                  "an array of pointers to machine code");
+  if (block_pointers == NULL)
     return (jitf *)SCM_BOOL_F;
-  
-  *code = (jitf *)buffer;
 
-  return code;
+  *block_pointers = code;
+
+  mprotect_res = mprotect (code, getpagesize(),
+                           PROT_READ | PROT_WRITE | PROT_EXEC);
+  if (mprotect_res != 0) {
+    perror("mprotect");
+    return (jitf *)SCM_BOOL_F;
+  }
+
+  return block_pointers;
 }
 
 void scm_init_jit (void)
